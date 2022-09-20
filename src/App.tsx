f6a8f29726862e45a1cda6fc6f0037f1064183ac
takacsberni <takacsberni@gmail.com>
2022-09-20 14:38:24 +0200

    feat: implement the handler function and give it a return type (void)

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,23 @@ const App: FC = () => {
     const [comment, setComment] = useState<string>("");
     const [todos, setTodos] = useState([]);
 
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        switch (event.target.name){
+            case "task":
+                setTask(event.target.value);
+                break;
+            case "comment":
+                setComment(event.target.value);
+                break;
+        }
     }
 
     return (
     <div className="App">
       <div className="header">
           <div className="inputContainer">
-              <input placeholder="Task" type="text" onChange={handleInputChange} />
-              <input placeholder="Comment " type="text" onChange={handleInputChange} />
+              <input placeholder="Task" type="text" onChange={handleInputChange} name="task"/>
+              <input placeholder="Comment " type="text" onChange={handleInputChange} name="comment" />
           </div>
           <button> Add a new task </button>
       </div>
