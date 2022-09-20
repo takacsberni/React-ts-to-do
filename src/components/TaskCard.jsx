14812436ab8df68afe6f7e303843f28d6ae9227f
takacsberni <takacsberni@gmail.com>
2022-09-20 15:55:20 +0200

    feat: fix TaskCards file extension and create a Props type inside of it

--- src/components/TaskCard.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const TaskCard = () => {
-   return <div key={}> something </div>
-};
---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {TaskType} from "../types/TaskType";
+
+type Props = {
+   task: TaskType;
+}
+
+export const TaskCard = ({task}: Props) => {
+   return <div key={}> something </div>
+};
