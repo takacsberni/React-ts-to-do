1cfd6cdd98c1d2dad9aa8c33656984cd3737f99c
takacsberni <takacsberni@gmail.com>
2022-09-20 14:18:02 +0200

    feat: create the basic of handleInputChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useState, ChangeEvent} from 'react';
 import './App.css';
 
 const App: FC = () => {
@@ -6,12 +6,15 @@ const App: FC = () => {
     const [comment, setComment] = useState<string>("");
     const [todos, setTodos] = useState([]);
 
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    }
+
     return (
     <div className="App">
       <div className="header">
           <div className="inputContainer">
-              <input placeholder="Task" type="text" />
-              <input placeholder="Comment " type="text"  />
+              <input placeholder="Task" type="text" onChange={handleInputChange} />
+              <input placeholder="Comment " type="text" onChange={handleInputChange} />
           </div>
           <button> Add a new task </button>
       </div>
