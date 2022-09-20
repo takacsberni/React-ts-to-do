3475364fa9aea566aa3ba0b7b7be86dc7160f9c4
takacsberni <takacsberni@gmail.com>
2022-09-20 13:13:14 +0200

    feat: create two input fields and a button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 const App: FC = () => {
   return (
     <div className="App">
-      <div className="header"></div>
+      <div className="header">
+          <div className="inputContainer">
+              <input placeholder="Task" type="text" />
+              <input placeholder="Comment " type="text"  />
+          </div>
+          <button> Add a new task </button>
+      </div>
       <div className="todos"></div>
 
     </div>
